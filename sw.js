da306318187452002992a1a058c159934e1df734
pxrcontacto <pxrcontacto@gmail.com>
2025-12-29 10:45:44 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,7 @@
-const CACHE_NAME = 'porxrazon-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
+const CACHE_NAME = "porxrazon-pwa-v1";
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["/","/?pwa=true"])));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
